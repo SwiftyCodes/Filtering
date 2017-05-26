5d49f3bca019fd6025065ceb922c1712f9d995e0
Polak <polak@Polaks-MacBook-Pro.local>
2017-05-26 17:28:40 +0530

    Initial Commit

---
+++ Filtering/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Filtering
+//
+//  Created by Polak on 5/26/17.
+//  Copyright © 2017 Polak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
